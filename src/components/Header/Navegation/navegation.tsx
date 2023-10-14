573588f0559d2f36776f3c089dd6f86131cfd70f
MarcosHogs97 <115594562+MarcosHogs97@users.noreply.github.com>
2023-10-13 23:12:03 -0300

    fist commit

---
+++ src/components/Header/Navegation/navegation.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function navegation() {
+    return (
+        <nav>
+            <ul>
+                <img src="" alt="Logo stark team" />
+                <a href="http://">Homme</a>
+                <a href="http://">Sobre</a>
+                <a href="http://">Artefatos</a>
+            </ul>
+        </nav>
+    )
+
+};
+
+export default navegation;
